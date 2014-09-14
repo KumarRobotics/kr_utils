5a1ac0e50112100602a7cae2c72329a438eac135
Chao Qu <quchao@seas.upenn.edu>
2014-09-14 06:51:31 -0400

    add rviz_helper

---
+++ rviz_helper/include/rviz_helper/visualizer.h
@@ -0,0 +1,124 @@
+#ifndef RVIZ_HELPER_VISUALIZER_H_
+#define RVIZ_HELPER_VISUALIZER_H_
+
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <rviz_helper/colors.h>
+
+namespace rviz_helper {
+
+class TrajectoryVisualizer {
+ public:
+  TrajectoryVisualizer(const ros::NodeHandle &nh,
+                       const std::string &topic = std::string("traj"));
+
+  void set_color(const rviz::Color &color) {
+    marker_.color.r = color.r_;
+    marker_.color.g = color.g_;
+    marker_.color.b = color.b_;
+  }
+  void set_alpha(double alpha) { marker_.color.a = alpha; }
+  void set_scale(double scale) { marker_.scale.x = scale; }
+  void set_lifetime(const ros::Duration &duration) {
+    marker_.lifetime = duration;
+  }
+  void set_num_skip(int num_skip) { num_skip_ = num_skip; }
+
+  void PublishTrajectory(const geometry_msgs::Pose &pose,
+                         const std_msgs::Header &header);
+  void PublishTrajectory(const geometry_msgs::Point &point,
+                         const std_msgs::Header &header);
+  void PublishTrajectory(const geometry_msgs::Point &point,
+                         const std::string &frame_id, const ros::Time &stamp);
+
+ private:
+  ros::NodeHandle nh_;
+  ros::Publisher traj_pub_;
+  int num_skip_;
+  int total_points_cnt_;
+  visualization_msgs::Marker marker_;
+};
+
+/**
+ * @brief Visualize covariance as a scaled ellipsoid.
+ */
+class CovarianceVisualizer {
+ public:
+  CovarianceVisualizer(const ros::NodeHandle &nh,
+                       const std::string &topic = std::string("covariance"));
+
+  void set_color(const rviz::Color &color) {
+    marker_.color.r = color.r_;
+    marker_.color.g = color.g_;
+    marker_.color.b = color.b_;
+  }
+  void set_alpha(double alpha) { marker_.color.a = alpha; }
+
+  void PublishCovariance(const geometry_msgs::PoseWithCovariance &pose_cov,
+                         const std_msgs::Header &header);
+  void PublishCovariance(
+      const geometry_msgs::PoseWithCovarianceStamped &pose_cov_stamped);
+  void PublishCovariance(const nav_msgs::Odometry &odometry);
+
+ private:
+  ros::NodeHandle nh_;
+  ros::Publisher cov_pub_;
+  visualization_msgs::Marker marker_;
+};
+
+class TfPublisher {
+ public:
+  TfPublisher() = default;
+  TfPublisher(const std::string &child_frame_id)
+      : child_frame_id_(child_frame_id) {}
+
+  const std::string &child_frame_id() const { return child_frame_id_; }
+  void set_child_frame_id(const std::string &id) { child_frame_id_ = id; }
+
+  void PublishTransform(const geometry_msgs::Pose &pose,
+                        const std_msgs::Header &header);
+  void PublishTransform(const geometry_msgs::Pose &pose,
+                        const std::string &frame_id, const ros::Time &stamp);
+
+ private:
+  std::string child_frame_id_;
+  tf2_ros::TransformBroadcaster tf_broadcaster_;
+};
+
+class PoseVisualizer {
+ public:
+  PoseVisualizer(const ros::NodeHandle &nh, const std::string &topic = "traj",
+                 const std::string &child_frame_id = "")
+      : nh_(nh), traj_viz_(nh, topic), tf_pub_(child_frame_id) {}
+
+  void set_color(const rviz::Color &color) { traj_viz_.set_color(color); }
+  void set_alpha(double alpha) { traj_viz_.set_alpha(alpha); }
+  void set_scale(double scale) { traj_viz_.set_scale(scale); }
+
+  const std::string &child_frame_id() const { return tf_pub_.child_frame_id(); }
+  void set_child_frame_id(const std::string &frame_id) {
+    tf_pub_.set_child_frame_id(frame_id);
+  }
+
+  void PublishPose(const geometry_msgs::Pose &pose,
+                   const std_msgs::Header &header);
+  void PublishPose(const geometry_msgs::Pose &pose, const std::string &frame_id,
+                   const ros::Time &stamp);
+
+ private:
+  ros::NodeHandle nh_;
+  TrajectoryVisualizer traj_viz_;
+  TfPublisher tf_pub_;
+};
+
+}  // namespace rviz_helper
+
+#endif  // RVIZ_HELPER_VISUALIZER_H_
---
+++ rviz_helper/src/visualizer.cpp
@@ -0,0 +1,112 @@
+#include "rviz_helper/visualizer.h"
+
+namespace rviz_helper {
+
+TrajectoryVisualizer::TrajectoryVisualizer(const ros::NodeHandle &nh,
+                                           const std::string &topic)
+    : nh_{nh},
+      traj_pub_(nh_.advertise<visualization_msgs::Marker>(topic, 1)),
+      num_skip_(1),
+      total_points_cnt_(0) {
+  marker_.pose.orientation.w = 1.0;
+  set_color(colors::RED);
+  set_scale(0.05);
+  marker_.action = visualization_msgs::Marker::ADD;
+  marker_.type = visualization_msgs::Marker::LINE_STRIP;
+  marker_.lifetime = ros::Duration();
+}
+
+void TrajectoryVisualizer::PublishTrajectory(const geometry_msgs::Pose &pose,
+                                             const std_msgs::Header &header) {
+  PublishTrajectory(pose.position, header);
+}
+
+void TrajectoryVisualizer::PublishTrajectory(const geometry_msgs::Point &point,
+                                             const std_msgs::Header &header) {
+  PublishTrajectory(point, header.frame_id, header.stamp);
+}
+
+void TrajectoryVisualizer::PublishTrajectory(const geometry_msgs::Point &point,
+                                             const std::string &frame_id,
+                                             const ros::Time &stamp) {
+  ++total_points_cnt_;
+  if (total_points_cnt_ % num_skip_) return;
+  marker_.points.push_back(point);
+  marker_.header.frame_id = frame_id;
+  marker_.header.stamp = stamp;
+  traj_pub_.publish(marker_);
+}
+
+CovarianceVisualizer::CovarianceVisualizer(const ros::NodeHandle &nh,
+                                           const std::string &topic)
+    : nh_(nh), cov_pub_(nh_.advertise<visualization_msgs::Marker>(topic, 1)) {
+  //  default to a beige colour
+  set_color({1, 0.9255, 0.5});
+
+  marker_.action = visualization_msgs::Marker::ADD;
+  marker_.type = visualization_msgs::Marker::SPHERE;
+  marker_.lifetime = ros::Duration();  //  last forever
+}
+
+void CovarianceVisualizer::PublishCovariance(
+    const geometry_msgs::PoseWithCovariance &pose_cov,
+    const std_msgs::Header &header) {
+  marker_.header = header;
+  marker_.pose.position = pose_cov.pose.position;
+  // Should orientation be in body frame?
+  marker_.pose.orientation.w = 1;
+
+  double scale[3] = {0, 0, 0};
+  for (int i = 0; i < 3; i++) {
+    if (pose_cov.covariance[(i * 6) + i] >= 0) {
+      scale[i] = std::sqrt(pose_cov.covariance[(i * 6) + i]);
+    }
+  }
+
+  marker_.scale.x = scale[0];
+  marker_.scale.y = scale[1];
+  marker_.scale.z = scale[2];
+  cov_pub_.publish(marker_);
+}
+
+void CovarianceVisualizer::PublishCovariance(
+    const geometry_msgs::PoseWithCovarianceStamped &pose_cov_stamped) {
+  PublishCovariance(pose_cov_stamped.pose, pose_cov_stamped.header);
+}
+
+void CovarianceVisualizer::PublishCovariance(
+    const nav_msgs::Odometry &odometry) {
+  PublishCovariance(odometry.pose, odometry.header);
+}
+
+void TfPublisher::PublishTransform(const geometry_msgs::Pose &pose,
+                                   const std_msgs::Header &header) {
+  PublishTransform(pose, header.frame_id, header.stamp);
+}
+
+void TfPublisher::PublishTransform(const geometry_msgs::Pose &pose,
+                                   const std::string &frame_id,
+                                   const ros::Time &stamp) {
+  geometry_msgs::Vector3 translation;
+  translation.x = pose.position.x;
+  translation.y = pose.position.y;
+  translation.z = pose.position.z;
+
+  geometry_msgs::TransformStamped transform_stamped;
+  transform_stamped.header.frame_id = frame_id;
+  transform_stamped.header.stamp = stamp;
+  transform_stamped.child_frame_id = child_frame_id_;
+  transform_stamped.transform.translation = translation;
+  transform_stamped.transform.rotation = pose.orientation;
+
+  tf_broadcaster_.sendTransform(transform_stamped);
+}
+
+void PoseVisualizer::PublishPose(const geometry_msgs::Pose &pose,
+                                 const std::string &frame_id,
+                                 const ros::Time &stamp) {
+  traj_viz_.PublishTrajectory(pose.position, frame_id, stamp);
+  tf_pub_.PublishTransform(pose, frame_id, stamp);
+}
+
+}  // namespace rviz_helper
