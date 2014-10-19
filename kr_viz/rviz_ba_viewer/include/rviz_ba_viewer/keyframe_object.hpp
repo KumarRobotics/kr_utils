d934c3520fd46130e1309dacef0de151b1bd292d
Gareth Cross <gcross.code@icloud.com>
2014-10-19 19:53:14 -0400

    added class for drawing the frustum and image, dont know if it works yet

--- kr_viz/rviz_ba_viewer/include/rviz_ba_viewer/keyframe_object.hpp
+++ kr_viz/rviz_ba_viewer/include/rviz_ba_viewer/keyframe_object.hpp
@@ -9,7 +9,12 @@
 #include <OGRE/OgreTexture.h>
 #include <OGRE/OgreMaterial.h>
 #include <OGRE/OgreVector3.h>
+#include <OGRE/OgreVector4.h>
 #include <OGRE/OgreQuaternion.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <image_geometry/pinhole_camera_model.h>
 
 namespace Ogre {
 class SceneManager;
@@ -22,17 +27,57 @@ public:
   
   KeyFrameObject(Ogre::SceneManager * scene_manager, int id);
   virtual ~KeyFrameObject();
-    
+  
   const int& id() const { return id_; }
   
+  /**
+   * @brief Set camera image.
+   * @param image
+   */
+  void setImage(const cv::Mat& image);
+  
+  /**
+   * @brief Set camera information.
+   * @param model
+   * @param width
+   * @param height
+   */
+  void setCameraModel(const image_geometry::PinholeCameraModel& model,
+                      int width, int height);
+  
+  /**
+   * @brief Set pose in R3.
+   * @param position
+   * @param orientation
+   */
   void setPose(const Ogre::Vector3& position,
                const Ogre::Quaternion& orientation);
   
+  /**
+   * @brief Set scale of frustum.
+   * @param scale
+   */
+  void setScale(double scale) { scale_ = scale; }
+  
+  /**
+   * @brief Set color of the frustum.
+   * @param color
+   */
+  void setColor(const Ogre::Vector4& color) { color_ = color; }
+  
+  /**
+   * @brief Create objects for rendering, call from update().
+   */
   void createGeometry();
   
 private:
   
   const int id_;
+  cv::Mat image_;
+  image_geometry::PinholeCameraModel cam_model_;
+  float width_{0}, height_{0};
+  double scale_{1};
+  Ogre::Vector4 color_{1,1,1,1};
   
   /// Ogre objects
   Ogre::SceneManager * scene_manager_;
--- kr_viz/rviz_ba_viewer/src/keyframe_object.cpp
+++ kr_viz/rviz_ba_viewer/src/keyframe_object.cpp
@@ -10,6 +10,24 @@
 #include <OGRE/OgreSceneManager.h>
 #include <OGRE/OgreSceneNode.h>
 
+Ogre::TexturePtr textureFromMat(const cv::Mat& mat, const std::string& name) {
+  assert(!mat.empty());
+  //  place mat data into a stream for ogre
+  Ogre::TexturePtr texture;
+  Ogre::DataStreamPtr data_stream;
+  data_stream.bind(new Ogre::MemoryDataStream((void *)mat.data,
+                                              mat.total() * mat.elemSize()));
+
+  const Ogre::String res_group =
+      Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME;
+  Ogre::TextureManager &texture_manager = Ogre::TextureManager::getSingleton();
+  //  swap byte order when going to Ogre
+  texture = texture_manager.loadRawData(name, res_group, data_stream,
+                                        mat.cols, mat.rows,
+                                        Ogre::PF_B8G8R8, Ogre::TEX_TYPE_2D, 0);
+  return texture;
+}
+
 KeyFrameObject::KeyFrameObject(Ogre::SceneManager * scene_manager, int id) :
   id_(id), scene_manager_(scene_manager) {
   
@@ -17,6 +35,20 @@ KeyFrameObject::KeyFrameObject(Ogre::SceneManager * scene_manager, int id) :
   scene_node_ = scene_manager_->createSceneNode();
   frustum_object_= scene_manager_->createManualObject();
   plane_object_ = scene_manager_->createManualObject();
+  scene_node_->attachObject(frustum_object_);
+  scene_node_->attachObject(plane_object_);
+  
+  //  create a material
+  const Ogre::String& group = 
+      Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME;
+  Ogre::MaterialManager& man = Ogre::MaterialManager::getSingleton();
+  //  some boring settings...
+  material_ = man.create("kf_mat_" + std::to_string(id), group);
+  material_->setReceiveShadows(false);
+  material_->getTechnique(0)->setLightingEnabled(false);
+  material_->setCullingMode(Ogre::CULL_NONE);
+  material_->setDepthWriteEnabled(true);
+  material_->setSceneBlending(Ogre::SBT_TRANSPARENT_ALPHA);
 }
 
 KeyFrameObject::~KeyFrameObject() {
@@ -27,6 +59,23 @@ KeyFrameObject::~KeyFrameObject() {
   scene_manager_->destroyManualObject(plane_object_);
 }
 
+void KeyFrameObject::setImage(const cv::Mat& image) {
+  image_ = image;
+  //  also re-create the ogre texture
+  texture_ = textureFromMat(image, "kf_tex_" + std::to_string(id_));
+  std::cout << "Created texture " << texture_->getName() << "\n";
+  std::cout << "Dimensions: " << texture_->getWidth() << ","
+            << texture_->getHeight() << std::endl;
+}
+
+void KeyFrameObject::setCameraModel(
+    const image_geometry::PinholeCameraModel& model,
+    int width, int height) {
+  cam_model_ = model;
+  width_ = width;
+  height_ = height;
+}
+
 void KeyFrameObject::setPose(const Ogre::Vector3& position,
                              const Ogre::Quaternion& orientation) {
   scene_node_->setPosition(position);
@@ -35,4 +84,72 @@ void KeyFrameObject::setPose(const Ogre::Vector3& position,
 
 void KeyFrameObject::createGeometry() {
   
+  const double fx = cam_model_.fx(), fy = cam_model_.fy();
+  const double cx = cam_model_.cx(), cy = cam_model_.cy();
+  //  generate coordinates of the frustum
+  Ogre::Vector3 points[] = {{0,0,1},            //  top left
+                            {0,height_,1},      //  bottom left
+                            {width_,height_,1}, //  bottom right
+                            {width_,0,1}};      //  top right
+  for (int i=0; i < 4; i++) {
+    points[i].x = (points[i].x - cx) / fx;
+    points[i].y = (points[i].y - cy) / fy;
+    points[i] *= scale_;
+  }
+  const Ogre::Vector3 centre(0,0,0);  //  optical centre
+  
+  frustum_object_->begin(material_->getName(),
+                         Ogre::RenderOperation::OT_LINE_LIST);
+  {
+    frustum_object_->colour(color_.x,color_.y,color_.z,color_.w);
+    //  left side
+    frustum_object_->position(points[0]);
+    frustum_object_->position(centre);
+    frustum_object_->position(centre);
+    frustum_object_->position(points[1]);
+    //  right side
+    frustum_object_->position(points[2]);
+    frustum_object_->position(centre);
+    frustum_object_->position(centre);
+    frustum_object_->position(points[3]);
+    //  front rectangle
+    for (int i=0; i < 4; i++) {
+      frustum_object_->position(points[i]);
+      frustum_object_->position(points[(i+1) % 4]);
+    }
+  }
+  frustum_object_->end();
+  
+  if (texture_->isLoaded()) {
+    //  now the textured quad w/ our image
+    //  first set up the material for textured drawing
+    Ogre::Pass * pass = material_->getTechnique(0)->getPass(0);
+    Ogre::TextureUnitState * tex_unit;
+    if (pass->getNumTextureUnitStates() > 0) {
+      tex_unit = pass->getTextureUnitState(0);
+    } else {
+      tex_unit = pass->createTextureUnitState();
+    }
+    tex_unit->setTextureName(texture_->getName());
+    tex_unit->setTextureFiltering(Ogre::TFO_BILINEAR);
+    tex_unit->setAlphaOperation(Ogre::LBX_SOURCE1, Ogre::LBS_MANUAL,
+                                Ogre::LBS_CURRENT, color_.w); //  use alpha
+    
+    plane_object_->begin(material_->getName(),
+                         Ogre::RenderOperation::OT_TRIANGLE_LIST);
+    {
+      plane_object_->textureCoord(0,1);
+      plane_object_->position(points[0]);
+      
+      plane_object_->textureCoord(0,0);
+      plane_object_->position(points[1]);
+      
+      plane_object_->textureCoord(1,0);
+      plane_object_->position(points[2]);
+      
+      plane_object_->textureCoord(1,1);
+      plane_object_->position(points[3]);
+    }
+    plane_object_->end();
+  }
 }
