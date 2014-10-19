d1afc1630072202ce025d1a5818a7ef103ef6ef9
Gareth Cross <gcross.code@icloud.com>
2014-10-19 15:26:41 -0400

    Modifying camera draw mechanism

--- kr_viz/include/kr_viz/qglviewer/camera.hpp
+++ kr_viz/include/kr_viz/qglviewer/camera.hpp
@@ -3,17 +3,35 @@
 
 #include <QGLViewer/camera.h>
 #include <opencv2/core/core.hpp>
+#include <kr_viz/qglviewer/texture.hpp>
 
 namespace kr {
 namespace viz {
 
 class Camera : public qglviewer::Camera {
  public:
-  void set_image(const cv::Mat& image) { image_ = image; }
+  void set_image(const cv::Mat& image) { 
+    image_ = image;
+    image_dirty_= true;
+  }
 
+  /**
+   * @brief draw
+   * @param drawFarPlane Unused in this implementation.
+   * @param scale Distance to the image plane, recommended = 1.
+   */
+  virtual void draw(bool drawFarPlane=true, float scale=1.0) const;
+  
+  void setFrustumColor(const qglviewer::Vec& c) { frustumColor_ = c; }
+  
  private:
   bool display_srt_{false};
+  qglviewer::Vec frustumColor_{1,1,1};
+  
   cv::Mat image_;
+  mutable bool image_dirty_{false};
+  mutable Texture::Ptr texture_;
+  
   cv::Matx33d K_;
   double width_{}, height_{};
 };
