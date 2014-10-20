d4c72b6077744c5b7909365fd668f416599b6963
Chao Qu <quchao@seas.upenn.edu>
2014-10-19 21:14:58 -0400

    add msg for rviz_ba_viewer

---
+++ kr_viz/rviz_helper/include/rviz_helper/colors.h
@@ -0,0 +1,21 @@
+#ifndef RVIZ_HELPER_COLROS_H_
+#define RVIZ_HELPER_COLROS_H_
+
+#include <rviz/helpers/color.h>
+
+namespace kr {
+namespace rviz_helper {
+namespace colors {
+
+const rviz::Color RED = {1, 0, 0};
+const rviz::Color GREEN = {0, 1, 0};
+const rviz::Color BLUE = {0, 0, 1};
+const rviz::Color CYAN = {0, 1, 1};
+const rviz::Color MAGENTA = {1, 0, 1};
+const rviz::Color YELLOW = {1, 1, 0};
+
+}  // namespace colors
+}  // namespace rviz_helper
+}  // namespace kr
+
+#endif  // RVIZ_HELPER_COLROS_H_
