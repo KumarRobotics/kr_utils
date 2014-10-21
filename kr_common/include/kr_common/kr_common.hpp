21711a94602ad21f254d3d8ccd92a04a69af1444
Chao Qu <quchao@seas.upenn.edu>
2014-10-20 23:02:26 -0400

    resolve param name in getParam

---
+++ kr_common/include/kr_common/kr_common.hpp
@@ -0,0 +1,10 @@
+#ifndef KR_COMMON_HPP_
+#define KR_COMMON_HPP_
+
+#include <kr_common/kr_common.hpp>
+#include <kr_common/pretty_print.hpp>
+#include <kr_common/pretty_printer.hpp>
+#include <kr_common/ros_helper.hpp>
+#include <kr_common/timer.hpp>
+
+#endif // KR_COMMON_HPP_
