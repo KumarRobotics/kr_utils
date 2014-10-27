e56feadd9e8b1b9b257b5c64b95bd6d2ae0b3e10
Chao Qu <quchao@seas.upenn.edu>
2014-10-26 23:32:55 -0400

    need to rewrite uniform with sifane

--- kr_common/include/kr_common/sample.hpp
+++ kr_common/include/kr_common/sample.hpp
@@ -4,6 +4,36 @@
 namespace kr {
 namespace common {
 
+#include <random>
+#include <chrono>
+
+class Sample {
+ public:
+  static int Uniform(int upper, int lower) {
+    std::uniform_int_distribution<int> distribution(upper, lower);
+    return distribution(generator_int);
+  }
+
+  /**
+   * @brief Uniform distribution real
+   */
+  template <typename T>
+  static T Uniform() {
+    std::uniform_real_distribution<T> distribution(0.0, 1.0);
+    return distribution(generator_real);
+  }
+
+  /**
+   * @brief Gaussian distribution
+   */
+  template <typename T>
+  static T Gaussian(T mean = 0.0, T stddev = 1.0) {
+    std::normal_distribution<double> distribution(mean, stddev);
+    return distribution(generator_real);
+  }
+
+ private:
+};
 
 }  // namespace common
 }  // namespace kr
